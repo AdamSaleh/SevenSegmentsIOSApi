4a9401267bb8a408e161215fc8c8a65f62c5d882
adam <adamthecamper@gmail.com>
2015-01-10 22:17:53 +0100

    Initial Commit

---
+++ SevenSegmentsApi/SevenSegmentsApi.h
@@ -0,0 +1,13 @@
+//
+//  SevenSegmentsApi.h
+//  SevenSegmentsApi
+//
+//  Created by Adam Saleh on 10/01/15.
+//  Copyright (c) 2015 Adam Saleh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SevenSegmentsApi : NSObject
+
+@end
---
+++ SevenSegmentsApi/SevenSegmentsApi.m
@@ -0,0 +1,13 @@
+//
+//  SevenSegmentsApi.m
+//  SevenSegmentsApi
+//
+//  Created by Adam Saleh on 10/01/15.
+//  Copyright (c) 2015 Adam Saleh. All rights reserved.
+//
+
+#import "SevenSegmentsApi.h"
+
+@implementation SevenSegmentsApi
+
+@end
